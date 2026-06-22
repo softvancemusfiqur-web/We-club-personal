cc1edeeaebfb550bc4211d5e389deead38c521c2
Musfiqur Rahman <softvancemusfiqur@gmail.com>
2026-06-22 16:01:38 +0600

    updated the inital api response

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World from Adam Server bro!");
 })
 
 app.use("/api/v1", router);
