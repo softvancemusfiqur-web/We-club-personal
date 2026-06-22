40314e3775ceedae6ba65d18e28bb0e96bcdebe5
Musfiqur Rahman <softvancemusfiqur@gmail.com>
2026-06-22 12:48:29 +0600

    removed some unused code

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,29 +1,9 @@
 import { NextFunction, Request, Response, Router } from "express";
-import User from "./users.model.schema";
-import sendResponse from "../../utility/sendResponse";
 import { userController } from "./user.controller";
 
 const router = Router();
 
 router.get("/", userController.getAllUsers);
-
-// router.get("/", async (req : Request, res : Response, next : NextFunction) => {
-//   try {
-
-//     const  users = await User.find();
-
-
-//     sendResponse(res, {
-//         statusCode : 200,
-//         sucess: true,  
-//         message : "Users retrieved successfully",
-//         data : users
-//     })
-
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 export const userRoutes = router;
 
